3c8f488bf9e034523aa4a9ea441c504b4b5b346b
udoh <udo@hoefel.eu>
2022-04-22 21:05:40 +0200

    javadoc fixes

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,17 +1,17 @@
 /**
  * This module provides support for programmatic (generic) coordinate systems,
- * such as {@link CartesianCoordinates}.
+ * such as {@link eu.hoefel.coordinates.CartesianCoordinates}.
  * <p>
  * The {@link eu.hoefel.coordinates} is the package containing the base classes
  * needed for the handling of coordinate systems, as well as some basic
  * implementations, e.g. for Cartesian, cylindrical and spherical coordinate
  * systems. It requires both the {@link eu.hoefel.coordinates.axes} and
  * {@link eu.hoefel.coordinates.tensors} packages, as well as the external
- * dependency {@link eu.hoefel.units} to manage the units on the axes.
+ * dependency {@link eu.hoefel.unit} to manage the units on the axes.
  * <p>
  * The {@link eu.hoefel.coordinates.axes} is the package containing axes related
  * classes necessary for each coordinate system. It requires the external
- * dependency {@link eu.hoefel.units} to manage the units on the axes.
+ * dependency {@link eu.hoefel.unit} to manage the units on the axes.
  * <p>
  * The {@link eu.hoefel.coordinates.tensors} is the package containing classes
  * for basic tensor transformations.
